73743a68e0d4c87d237809c15101618683654da9
Gustavo <gustavo@MacBook-Air-de-Kerad.local>
2017-04-27 13:52:25 +0200

    add serie model

--- app/models/serie.js
+++ app/models/serie.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    poster_path: DS.attr('string'),
+    popularity: DS.attr('number'),
+    backdrop_path: DS.attr('string'),
+    vote_average: DS.attr('number'),
+    overview: DS.attr('string'),
+    first_air_date: DS.attr('string'),
+    vote_count: DS.attr('number'),
+    name: DS.attr('string'),
+    original_name: DS.attr('string')
 });
