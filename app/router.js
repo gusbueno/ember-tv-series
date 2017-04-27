fde7657df06005b9dd03cb1736470a4595739f7f
Gustavo <gustavo@MacBook-Air-de-Kerad.local>
2017-04-27 10:34:58 +0200

    add header and search-bar

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('search');
+  this.route('serie-detail');
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+      //production
   }
 
   return ENV;
