b0bcccca9069c593d698163e28f1b3f8759f62db
Gustavo <gustavo@MacBook-Air-de-Kerad.local>
2017-04-27 17:58:49 +0200

    input with handle function

--- app/components/search-bar.js
+++ app/components/search-bar.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    init() {
+        this._super(...arguments);
+        //this.get('filter')('').then((results) => this.set('results', results));
+    },
+
+    actions: {
+        handleSearch(term) {
+            console.log(term);
+        },
+        /*
+        handleFilterEntry() {
+            let filterInputValue = this.get('value');
+            let filterAction = this.get('filter');
+            filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+        }
+        */
+    }
+
 });
